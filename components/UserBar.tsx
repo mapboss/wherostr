6b5900ea19f5765c604d96116e0ee6d263be89f9
Suttikeat Witchayakul <switchayakul@gmail.com>
2023-09-01 19:47:00 +0700

    Added user details

--- components/UserBar.tsx
+++ components/UserBar.tsx
@@ -1,7 +1,22 @@
 'use client'
 import { AccountContext } from '@/contexts/AccountContext'
-import { Button } from '@mui/material'
-import { useCallback, useContext } from 'react'
+import { Login, Logout } from '@mui/icons-material'
+import { Avatar, Button, IconButton, Typography } from '@mui/material'
+import { NDKUserProfile } from '@nostr-dev-kit/ndk'
+import { useCallback, useContext, useMemo } from 'react'
+
+const UserChip = ({ profile }: { profile: NDKUserProfile }) => {
+  const name = useMemo(() => profile.displayName || profile.name, [profile])
+  return (
+    <div className="flex">
+      <Avatar alt={name} src={profile.image} />
+      <div className="flex flex-col pl-2">
+        <Typography variant="subtitle2">{name}</Typography>
+        <Typography variant="caption">{profile.nip05}</Typography>
+      </div>
+    </div>
+  )
+}
 
 const UserBar = () => {
   const { user, signIn, signOut } = useContext(AccountContext)
@@ -13,22 +28,30 @@ const UserBar = () => {
   }, [signOut])
   return (
     <div
-      className="grid items-center p-3 rounded-bl-lg"
+      className="grid items-center p-3 rounded-bl-lg h-16"
       style={{
         background: 'linear-gradient(45deg, #dd262b 30%, #ffd700 90%)',
       }}
     >
-      {user ? (
+      {user?.profile ? (
+        <div className="flex items-center">
+          <UserChip profile={user.profile} />
+          <IconButton
+            classes={{ root: '!ml-2' }}
+            color="error"
+            size="small"
+            onClick={handleClickSignOut}
+          >
+            <Logout />
+          </IconButton>
+        </div>
+      ) : (
         <Button
           variant="contained"
-          color="secondary"
-          size="small"
-          onClick={handleClickSignOut}
+          color="primary"
+          onClick={handleClickSignIn}
+          endIcon={<Login />}
         >
-          Sign Out
-        </Button>
-      ) : (
-        <Button variant="contained" size="small" color='primary' onClick={handleClickSignIn}>
           Sign In
         </Button>
       )}
