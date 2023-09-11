31e6284ed2e4386ce687183e22109098cfda80bf
Nicky Dev <passapol.e@mapboss.co.th>
2023-09-11 11:09:45 +0700

    Added widget layout

---
+++ app/widgets/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Wherostr - Widgets',
+  description: '',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return children
+}
