50a52117273798a18df48497e5bf012299f6a9fd
Nicky Dev <passapol.e@mapboss.co.th>
2023-10-08 20:03:31 +0700

    Added next-pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const isProduction = process.env.NODE_ENV === "production"
+
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    skipWaiting: true,
+    register: isProduction,
+    disable: !isProduction,
+});
+
+
+const nextConfig = withPWA({
     output: "export",
     trailingSlash: true,
     images: {
         unoptimized: true
     }
-}
+})
 
 module.exports = nextConfig
